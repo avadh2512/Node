2ab64d9d3136655a04b38c50e0e6786500633e43
avadh2512 <avadhsutariya2512@gmail.com>
2026-02-08 21:49:53 +0100

    Recommit

---
+++ Day-8/Backend/src/app.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const noteModel = require("./models/note.model")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+
+app.use(express.json())
+
+app.post("/api/notes", async (req, res) => {
+    const { title, discription } = req.body
+
+    const note = await noteModel.create({
+        title, discription
+    })
+    res.status(201).json({
+        message: "Note created Successfully", note
+    })
+
+})
+
+app.get("/api/notes", async (req, res) => {
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "Note fetched successfully", notes
+    })
+})
+
+
+app.delete("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message: "Note deleted successfully"
+    })
+})
+
+
+app.patch("/api/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const { discription } = req.body
+     await noteModel.findByIdAndUpdate(id, { discription })
+
+    res.status(200).json({
+        message: "Updated"
+    })
+})
+
+module.exports = app;
+
---
+++ Day-8/Frontend/notes/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [notes, setNotes] = useState([
+    {
+      title: "test title 1",
+      discription: "test discription 1",
+    },
+    {
+      title: "test title 2",
+      discription: "test discription 2",
+    },
+    {
+      title: "test title 3",
+      discription: "test discription 3",
+    },
+    {
+      title: "test title 4",
+      discription: "test discription 4",
+    },
+  ]);
+
+  axios.get('http://localhost:3000/api/notes').then((res) => {
+   setNotes(res.data.notes);
+  });
+
+  return (
+    <>
+      <div className="notes">
+        {notes.map((note) => {
+          return (
+            <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.discription}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
