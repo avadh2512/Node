50a6786d965ce8130fd81d8ff08a19edea0009dc
avadh2512 <avadhsutariya2512@gmail.com>
2026-02-23 16:25:45 +0100

    Solve Unique

--- Day-12/server.js
+++ Day-12/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
+
 connectToDB();
 app.listen(3000,()=>{
     console.log("server is live on port 3000");
--- Day-12/src/routes/auth.routs.js
+++ Day-12/src/routes/auth.routs.js
@@ -7,12 +7,19 @@ const authRouter = express.Router()
 authRouter.post("/register", async (req, res) => {
     const { email, name, password } = req.body
 
-   const user = await userModel.create({
+    const isUserAlreadyExists = await userModel.findOne({email})
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message:"User Already Exists"
+        })
+    }
+
+    const user = await userModel.create({
         email, password, name
     })
 
     res.status(201).json({
-        message:"user Register Successfully",
+        message: "user Register Successfully",
         user
     })
 })
